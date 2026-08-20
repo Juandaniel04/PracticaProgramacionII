7abbe769c10773ba01f402b3007b8d098e6228fa
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 18:29:29 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PracticaProgramacionII;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ejercicio01.Ejecutar();
+
+        Console.WriteLine();
+        Console.WriteLine("Presiona ENTER para terminar...");
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApp1/ejercicio01.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticaProgramacionII;
+
+public static class Ejercicio01
+{
+    public static void Ejecutar()
+    {
+        string nombre = "Juan Daniel";
+
+        Console.WriteLine("Bienvenido a Programación II");
+        Console.WriteLine($"Mi nombre es: {nombre}");
+    }
+}
