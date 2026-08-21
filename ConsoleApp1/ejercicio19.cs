07c0249c440a6e5ea617e048732837c10ead32fa
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 21:50:59 -0400

    Crear ejercicio 19

---
+++ ConsoleApp1/ejercicio19.cs
@@ -0,0 +1,28 @@
+﻿namespace PracticaProgramacionII;
+
+class Ejercicio19
+{
+    public static void Ejecutar()
+    {
+        Console.WriteLine("================================");
+        Console.WriteLine("       EJERCICIO 19");
+        Console.WriteLine("================================");
+
+        Console.Write("Ingrese el primer número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        if (numero1 % numero2 == 0)
+        {
+            Console.WriteLine("El primer número es divisible entre el segundo");
+        }
+        else
+        {
+            Console.WriteLine("El primer número no es divisible entre el segundo");
+        }
+
+        Console.WriteLine("================================");
+    }
+}
