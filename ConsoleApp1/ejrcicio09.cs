0db2bfc769d6b064c3ba62e3194835c4babecada
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 21:12:41 -0400

    Crear ejercicio 09

---
+++ ConsoleApp1/ejrcicio09.cs
@@ -0,0 +1,22 @@
+﻿namespace PracticaProgramacionII;
+
+class Ejercicio09
+{
+    public static void Ejecutar()
+    {
+        Console.WriteLine("================================");
+        Console.WriteLine("       EJERCICIO 09");
+        Console.WriteLine("================================");
+
+        Console.Write("Ingrese la cantidad de minutos: ");
+        int minutosTotales = int.Parse(Console.ReadLine());
+
+        int horas = minutosTotales / 60;
+        int minutos = minutosTotales % 60;
+
+        Console.WriteLine("Horas: " + horas);
+        Console.WriteLine("Minutos: " + minutos);
+
+        Console.WriteLine("================================");
+    }
+}
