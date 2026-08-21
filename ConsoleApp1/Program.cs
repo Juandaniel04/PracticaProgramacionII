5cc18db6d40e61dadef6a6d21e6d0dc94b1185bd
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 22:11:46 -0400

    Crear ejercicio 26

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ejercicio25.Ejecutar();
+        Ejercicio26.Ejecutar();
 
         Console.WriteLine();
         Console.WriteLine("Presiona ENTER para terminar...");
