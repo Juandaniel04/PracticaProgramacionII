7d6c7bd066cfa98e0b6b88a7f5d276050f9a9df3
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 21:53:10 -0400

    Crear ejercicio 20

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ejercicio19.Ejecutar();
+        Ejercicio20.Ejecutar();
 
         Console.WriteLine();
         Console.WriteLine("Presiona ENTER para terminar...");
---
+++ ConsoleApp1/ejercicio20.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticaProgramacionII;
+
+class Ejercicio20
+{
+    public static void Ejecutar()
+    {
+        Console.WriteLine("================================");
+        Console.WriteLine("       EJERCICIO 20");
+        Console.WriteLine("================================");
+
+        Console.Write("Ingrese las horas trabajadas en la semana: ");
+        int horas = int.Parse(Console.ReadLine());
+
+        if (horas > 40)
+        {
+            Console.WriteLine("Tiene horas extra");
+        }
+        else
+        {
+            Console.WriteLine("Jornada normal");
+        }
+
+        Console.WriteLine("================================");
+    }
+}
