a55dc5c437fb5ab10ea2766a9be15e6e2b8d3398
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 21:46:57 -0400

    Crear ejercicio 17

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ejercicio16.Ejecutar();
+        Ejercicio17.Ejecutar();
 
         Console.WriteLine();
         Console.WriteLine("Presiona ENTER para terminar...");
---
+++ ConsoleApp1/ejercicio17.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticaProgramacionII;
+
+class Ejercicio17
+{
+    public static void Ejecutar()
+    {
+        Console.WriteLine("================================");
+        Console.WriteLine("       EJERCICIO 17");
+        Console.WriteLine("================================");
+
+        Console.Write("Ingrese un año: ");
+        int anio = int.Parse(Console.ReadLine());
+
+        if (anio % 4 == 0)
+        {
+            Console.WriteLine("El año es bisiesto");
+        }
+        else
+        {
+            Console.WriteLine("El año no es bisiesto");
+        }
+
+        Console.WriteLine("================================");
+    }
+}
