9c238763b86eea6967cac4574a8c13ad4614c059
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 20:54:41 -0400

    Crear ejercicio 03

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ejercicio02.Ejecutar();
+        Ejercicio03.Ejecutar();
 
         Console.WriteLine();
         Console.WriteLine("Presiona ENTER para terminar...");
---
+++ ConsoleApp1/ejercicio03.cs
@@ -0,0 +1,22 @@
+﻿namespace PracticaProgramacionII;
+
+class Ejercicio03
+{
+    public static void Ejecutar()
+    {
+        const int ANIO_ACTUAL = 2026;
+
+        Console.WriteLine("================================");
+        Console.WriteLine("       EJERCICIO 03");
+        Console.WriteLine("================================");
+
+        Console.Write("Ingrese su año de nacimiento: ");
+        int anioNacimiento = int.Parse(Console.ReadLine());
+
+        int edad = ANIO_ACTUAL - anioNacimiento;
+
+        Console.WriteLine("Su edad aproximada es: " + edad + " años");
+
+        Console.WriteLine("================================");
+    }
+}
