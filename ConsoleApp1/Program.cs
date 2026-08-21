be9dd4186ce648fab1429ed1dcc6ad54cf1e4eb0
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 21:42:53 -0400

    Crear ejercicio 15

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ejercicio14.Ejecutar();
+        Ejercicio15.Ejecutar();
 
         Console.WriteLine();
         Console.WriteLine("Presiona ENTER para terminar...");
