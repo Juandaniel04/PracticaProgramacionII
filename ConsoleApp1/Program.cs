60363b86c06d1a0fba675274b79af3850cd67e83
Juandaniel04 <juandanielvillazon04@gmail.com>
2026-08-20 21:35:10 -0400

    Crear ejercicio 11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ejercicio10.Ejecutar();
+        Ejercicio11.Ejecutar();
 
         Console.WriteLine();
         Console.WriteLine("Presiona ENTER para terminar...");
---
+++ ConsoleApp1/ejercicio11.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticaProgramacionII;
+
+class Ejercicio11
+{
+    public static void Ejecutar()
+    {
+        Console.WriteLine("================================");
+        Console.WriteLine("       EJERCICIO 11");
+        Console.WriteLine("================================");
+
+        Console.Write("Ingrese su edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Es mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Es menor de edad");
+        }
+
+        Console.WriteLine("================================");
+    }
+}
